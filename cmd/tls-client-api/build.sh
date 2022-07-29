10414501a551a3a52ed4380f6cc9676eb912fb64
Bogdan Finn <bogdan.finn@googlemail.com>
2022-07-29 08:39:29 +0200

    build version 0.1.0;

--- cmd/tls-client-api/build.sh
+++ cmd/tls-client-api/build.sh
@@ -1,9 +1,13 @@
 #!/bin/sh
 
-GOOS=darwin GOARCH=arm64 go build -o ./../../dist/tls-client-api-darwin ./main.go
+echo 'Build OSX'
+GOOS=darwin GOARCH=arm64 go build -o ./../../dist/tls-client-api-darwin-$1 ./main.go
 
-GOOS=linux GOARCH=amd64 go build -o ./../../dist/tls-client-api-linux ./main.go
+echo 'Build Linux'
+GOOS=linux GOARCH=amd64 go build -o ./../../dist/tls-client-api-linux-$1 ./main.go
 
-GOOS=windows GOARCH=386 go build -o ./../../dist/tls-client-api-windows-32.exe ./main.go
+echo 'Build Windows 32 Bit'
+GOOS=windows GOARCH=386 go build -o ./../../dist/tls-client-api-windows-32-$1.exe ./main.go
 
-GOOS=windows GOARCH=amd64 go build -o ./../../dist/tls-client-api-windows-64.exe ./main.go
+echo 'Build Windows 64 Bit'
+GOOS=windows GOARCH=amd64 go build -o ./../../dist/tls-client-api-windows-64-$1.exe ./main.go
